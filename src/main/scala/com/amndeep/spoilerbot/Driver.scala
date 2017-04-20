9725ad127f36a1c5efeae8e3f30ed1486f14ff87
Amndeep Singh Mann <amndeep.dev@gmail.com>
2017-04-20 18:45:11 -0400

    What I finished last night

--- src/main/scala/com/amndeep/spoilerbot/Driver.scala
+++ src/main/scala/com/amndeep/spoilerbot/Driver.scala
@@ -3,13 +3,22 @@ package com.amndeep.spoilerbot
 import scala.io.Source
 
 import sx.blah.discord.api.ClientBuilder
+import sx.blah.discord.util.DiscordException
 
 object Driver {
   def login(token: String): Option[SpoilerBot] = {
     val builder = new ClientBuilder
     builder.withToken(token)
-    val client = builder.login
-    Option(new SpoilerBot(client))
+    try {
+      val client = builder.login
+      Some(new SpoilerBot(client))
+    }
+    catch {
+      case fail: DiscordException => {
+        println("Login failed because of " + fail)
+        None
+      }
+    }
   }
 
   def main(args: Array[String]): Unit = {
--- src/main/scala/com/amndeep/spoilerbot/SpoilerBot.scala
+++ src/main/scala/com/amndeep/spoilerbot/SpoilerBot.scala
@@ -1,7 +1,34 @@
 package com.amndeep.spoilerbot
 
 import sx.blah.discord.api.IDiscordClient
+import sx.blah.discord.api.events.EventSubscriber
+import sx.blah.discord.handle.impl.events.ReadyEvent
+import sx.blah.discord.handle.impl.events.guild.channel.message.MentionEvent
 
 class SpoilerBot(client: IDiscordClient) {
+  client.getDispatcher.registerListener(this)
 
+  @EventSubscriber
+  def onReady(event: ReadyEvent): Unit = {
+    println("Logged in")
+  }
+
+  // syntax: @bot replacement text == spoiler text
+  @EventSubscriber
+  def onMention(event: MentionEvent): Unit = {
+    println("Mentioned:\n\tby: " + event.getAuthor + "\n\tcontents: " + event.getMessage.getContent)
+
+    if(event.getAuthor.getLongID == client.getOurUser.getLongID) {
+      println("Don't do recursion")
+      return
+    }
+
+    val pattern = """^<@!?\d+> (.*) == (.*)""".r
+
+    val messages = pattern.findAllIn(event.getMessage.getContent).matchData.next.subgroups
+
+    event.getMessage.delete
+
+    event.getMessage.getChannel.sendMessage("I was mentioned by " + event.getAuthor.mention() + ", who wanted \"" + messages(1) + "\" replaced by \"" + messages(0) + "\"")
+  }
 }
