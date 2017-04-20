c6dff43f033e37f4c0f37b0d19464c5377cbd12c
Amndeep Singh Mann <amndeep.dev@gmail.com>
2017-04-20 19:40:05 -0400

    Changed try/catch to Try syntax in the driver

--- src/main/scala/com/amndeep/spoilerbot/Driver.scala
+++ src/main/scala/com/amndeep/spoilerbot/Driver.scala
@@ -1,30 +1,32 @@
 package com.amndeep.spoilerbot
 
 import scala.io.Source
+import scala.util.{Failure, Success, Try}
 
 import sx.blah.discord.api.ClientBuilder
-import sx.blah.discord.util.DiscordException
 
 object Driver {
-  def login(token: String): Option[SpoilerBot] = {
+  def login(token: String): Try[SpoilerBot] = {
     val builder = new ClientBuilder
     builder.withToken(token)
-    try {
+    Try({
       val client = builder.login
-      Some(new SpoilerBot(client))
-    }
-    catch {
-      case fail: DiscordException => {
-        println("Login failed because of " + fail)
-        None
-      }
-    }
+      new SpoilerBot(client)
+    })
   }
 
   def main(args: Array[String]): Unit = {
     val token: String = Source.fromFile("token").getLines.next()
     println(token)
-    val bot = login(token)
-    println(bot)
+    login(token) match {
+      case Failure(exception) => {
+        println("Login failed")
+        throw exception
+      }
+      case Success(bot) => {
+        println("Login succeeded")
+        println(bot)
+      }
+    }
   }
 }
